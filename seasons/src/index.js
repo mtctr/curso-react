1513bb5cdf2517ac31a028b3991c256b0bc73145
Mateus Cruz Targino <ti.mateus@liga.org.br>
2019-01-08 15:51:16 -0300

    section 6

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		//THIS IS THE ONLY TIME we do direct assignment to this.state
-		this.state = { lat: null, errorMessage: "" };
-	}
+	state = { lat: null, errorMessage: "" };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
@@ -26,10 +24,10 @@ class App extends React.Component {
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
-		return <div>Loading...</div>;
+		return <Spinner message="Please accept location request"/>;
 	}
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
