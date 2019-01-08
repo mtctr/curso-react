6f55d8021ce26639fddaa2a9d38972c7741660c8
Mateus Targino <mateustargino95@gmail.com>
2019-01-07 22:17:46 -0300

    use of lifecycle method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,22 +6,13 @@ class App extends React.Component {
 		super(props);
 		//THIS IS THE ONLY TIME we do direct assignment to this.state
 		this.state = { lat: null, errorMessage: "" };
-
-		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				//we called setstate!!!
-				this.setState({ lat: position.coords.latitude });
-			},
-			err => {
-				this.setState({
-					errorMessage: err.message
-				});
-			}
-		);
 	}
 
 	componentDidMount() {
-		console.log("My component was rendered to the screen");
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message })
+		);
 	}
 
 	componentDidUpdate() {
