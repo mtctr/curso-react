57a0ec6a5c0f510e24d26483cefe643c7fb16a08
Mateus Cruz Targino <ti.mateus@liga.org.br>
2019-01-28 09:59:14 -0300

    requests with axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
-const App = () => {
-	return (
-		<div className="ui container" style={{ marginTop: "10px" }}>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends React.Component {
+	onSearchSubmit(term) {
+		axios.get("https://api.unsplash.com/search/photos", {
+			params: {
+				query: term
+			},
+			headers: {
+				Authorization:
+					"Client-ID 05fd39bbcbc08d11a94988de16d861a6e4f227c46d6ebf26cd5ca0677955d73b"
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
 
 export default App;
