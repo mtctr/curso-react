56b9ea0d146fa6efe669f2b089d3239ec8cbe64a
Mateus Cruz Targino <ti.mateus@liga.org.br>
2020-03-06 16:48:49 -0300

    pics

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList.css";
 import React from "react";
-
+import ImageCard from "./ImageCard";
 const ImageList = props => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description}></img>;
-  });
-  return <div>{images}</div>;
+	const images = props.images.map(image => {
+		return <ImageCard key={image.id} image={image}></ImageCard>;
+	});
+	return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
