0be2e72b3f46715664f83e4e60c55ca8275a7212
Mateus Cruz Targino <ti.mateus@liga.org.br>
2020-03-12 16:44:11 -0300

    ..

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
 	state = { videos: [] };
@@ -17,7 +18,8 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="ui container">
-				<SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>
+				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
